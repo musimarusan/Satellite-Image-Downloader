14e92ad0b069c42fcb641ad3a29a2e99cf89be79
Your Name <musi.rusi@gmail.com>
2023-07-22 13:03:30 +0000

    investigation of geometry creation.

---
+++ Src/createGeomRect.py
@@ -0,0 +1,47 @@
+import numpy as np
+import geopandas as gpd
+
+
+def check_crs(gdf):
+    if gdf.crs is None:
+        return False
+    if gdf.crs.name == 'Undefined geographic SRS':
+        return False
+    return True
+
+
+polygon = './Boundary_Nagasu-town.gpkg'
+
+gdf = gpd.read_file(polygon)
+
+assert check_crs(gdf), 'CRS os not defined.'
+
+
+
+s = str(gdf['geometry'][0])
+
+
+s = s.replace('MULTIPOLYGON ','')
+s = s.replace('(((',' ')
+s = s.replace(')))','')
+
+lonlat = s.split(',')
+print(len(lonlat))
+#print(lonlat[0])
+
+nplonlat = np.array(lonlat)
+#print(nplonlat[10])
+
+lon = []
+lat = []
+for coord in lonlat:
+#    print(coord)
+    tmpcoord = coord.split(' ')
+#    print(tmpcoord)
+    lon.append(tmpcoord[1])
+    lat.append(tmpcoord[2])
+
+
+print(min(lon),max(lat))
+print(max(lon),min(lat))
+
